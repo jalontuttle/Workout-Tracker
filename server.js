9e9b2544f5fbc46569842ac9fe4dd842b48809e4
Jalon Tuttle <jalontuttle@gmail.com>
2021-12-03 17:41:14 -0500

    adding routes and connecting to Heroku

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const apiRoutes = require('./routes/apiroutes')
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
-const app = express();
+  const app = express();
 
 app.use(logger("dev"));
 
@@ -14,3 +23,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.use(apiRoutes);
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`App running on port ${PORT} `);
+});
